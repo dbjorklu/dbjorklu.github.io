423e8605d54753a064c8e384e562b8efb63be8c2
Dag Björklund <dag.bjorklund@comsel.fi>
2015-03-25 22:27:00 +0200

    carousel etc.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Initialise FlexSlider for Carousels
+$(window).load(function() {
+    $('.flexslider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    directionNav: false,
+    slideshowSpeed: 1000,
+    animationSpeed: 600,
+    touch: true
+    });
+});
+
